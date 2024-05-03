91f838a5f46be1b4a273424ab64f50eceeb42839
hamza4422 <168738333+hamza4422@users.noreply.github.com>
2024-05-03 15:45:01 +0300

    Update main.js

--- csci390 project/javascript/main.js
+++ csci390 project/javascript/main.js
@@ -1,10 +1,13 @@
-const images = ["image/image2.webp","image/image3.webp","image/image1.webp"];
+const images1 = ["image/image2.webp","image/image3.webp","image/image1.webp"];
+const images2 = ["image/image2.webp","image/image3.webp","image/image1.webp"];
+const images3 = ["image/image2.webp","image/image3.webp","image/image1.webp"];
+const images4 = ["image/image2.webp","image/image3.webp","image/image1.webp"];
 let currentIndex=0;
 function ChangeImage(){
  
- document.getElementById("ad-img").src=images[currentIndex];
-
-currentIndex= (currentIndex+1)%images.length;
+ document.getElementById("ad-img1").src=images1[currentIndex];
+ document.getElementById("ad-img2").src=images1[currentIndex];
+currentIndex= (currentIndex+1)%images1.length;
 
 }
 
